f45ca09e2892dddbd69cb2502764bfc15cdd9010
Trinh Duy Hai <haitrinh.se@gmail.com>
2021-08-16 00:19:53 +0700

    Singly Linked List

---
+++ src/com/haitd22/dsa/_3/_2/_1/SinglyLinkedList.java
@@ -0,0 +1,79 @@
+package com.haitd22.dsa._3._2._1;
+
+public class SinglyLinkedList<E> {
+    private Node<E> head = null;
+    private Node<E> tail = null;
+    private int size = 0;
+
+    public SinglyLinkedList() {
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public E first() {
+        if (isEmpty()) {
+            return null;
+        }
+        return head.element;
+    }
+
+    public E last() {
+        if (isEmpty()) {
+            return null;
+        }
+        return tail.element;
+    }
+
+    public void addFirst(E e) {
+        head = new Node<>(e, head);
+        if (size == 0) {
+            tail = head;
+        }
+        size++;
+    }
+
+    public void addLast(E e) {
+        Node<E> newest = new Node<>(e, null);
+        if (isEmpty()) {
+            head = newest;
+        } else {
+            tail.next = newest;
+        }
+        size++;
+        tail = newest;
+    }
+
+    public E removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        E removed = head.element;
+        head = head.getNext();
+        size--;
+        return removed;
+    }
+
+    private static class Node<E> {
+        private final E element;
+        private Node<E> next;
+
+        public Node(E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+    }
+}
